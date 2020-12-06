88dd7f96d1648494b88ef095785774e648daf0a8
Artem Puzankov <71190274+hellopuza@users.noreply.github.com>
2020-12-06 03:13:34 +0300

    Update Stack.h

--- Stack.h
+++ Stack.h
@@ -153,7 +153,7 @@ error_t TEMPLATE(StackExpand, TYPE) (TEMPLATE(stack, TYPE)* p_stk);
 error_t TEMPLATE(StackDump, TYPE) (TEMPLATE(stack, TYPE)* p_stk, const char* funcname = "@some function@", const char* logfile = logname);
 
 //------------------------------------------------------------------------------
-/*! @brief   Ñheck stack for problems, canaries, hash (if enabled)
+/*! @brief   Check stack for problems, canaries, hash (if enabled)
  *
  *  @param   p_stk       Pointer to stack
  *  @param   funcname    Name of the function from which the StackCheck was called
@@ -177,7 +177,7 @@ void TEMPLATE(printError, TYPE) (TEMPLATE(stack, TYPE)* p_stk, FILE* fp);
 //------------------------------------------------------------------------------
 /*! @brief   Change canary (if enabled)
  *
- *  @param   canary      Ñanary input
+ *  @param   canary      Ã‘anary input
  *
  *  @return  changed canary
  */
@@ -201,7 +201,7 @@ void TEMPLATE(CanaryPlacing, TYPE) (TEMPLATE(stack, TYPE)* p_stk);
 #endif // CANARY_PROTECT
 
 //------------------------------------------------------------------------------
-/*! @brief   Ñheck stack for canaries (if enabled)
+/*! @brief   Check stack for canaries (if enabled)
  *
  *  @param   p_stk       Pointer to stack
  *  @param   funcname    Name of the function from which the StackCheck was called
